245af7c8ae14498e5d8b929d41a336a9db3038b6
Joseph Talon <josephtalon01@gmail.com>
2025-08-09 06:39:48 -0600

    Remove safety that did not work

--- dynajoy/dynajoy/command_publisher.py
+++ dynajoy/dynajoy/command_publisher.py
@@ -37,8 +37,10 @@ MAX_PWM = 250
 
 # Default motor start positions
 RESTING_POINT = 1750
-position_wrist1 = RESTING_POINT
-position_wrist2 = RESTING_POINT
+# position_wrist1 = RESTING_POINT
+# position_wrist2 = RESTING_POINT
+velocity_wrist1 = 0
+velocity_wrist2 = 0
 velocity_shoulder1 = 0
 velocity_shoulder2 = 0
 velocity_elbow = 0
@@ -54,7 +56,7 @@ class DynamixelTeleop(Node):
             Joy,
             '/joy',
             self.joy_callback,
-            10
+            1
         )
 
         # Setup Dynamixel
@@ -87,12 +89,12 @@ class DynamixelTeleop(Node):
         )
 
         # Set max movement speed
-        dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-        	self.port_handler, WRIST1_ID, ADDR_GOAL_PWM, MAX_PWM
-        )
-        dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-        	self.port_handler, WRIST2_ID, ADDR_GOAL_PWM, MAX_PWM
-        )
+        # dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
+        # 	self.port_handler, WRIST1_ID, ADDR_GOAL_PWM, MAX_PWM
+        # )
+        # dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
+        # 	self.port_handler, WRIST2_ID, ADDR_GOAL_PWM, MAX_PWM
+        # )
 
         if dxl_comm_result != COMM_SUCCESS:
             self.get_logger().error(f"Enable torque failed: {self.packet_handler.getTxRxResult(dxl_comm_result)}")
@@ -102,7 +104,7 @@ class DynamixelTeleop(Node):
         self.get_logger().info("Dynamixel ready. Use joystick to control.")
 
     def joy_callback(self, joy_msg):
-        global position_wrist1, position_wrist2, velocity_elbow, velocity_shoulder1, velocity_shoulder2, velocity_elbow
+        global velocity_wrist1, velocity_wrist2, velocity_elbow, velocity_shoulder1, velocity_shoulder2, velocity_elbow
         x_axis = joy_msg.axes[0]  # Left/right
         y_axis = -joy_msg.axes[1]  # Forward/backward
         # Right joystick
@@ -114,47 +116,82 @@ class DynamixelTeleop(Node):
         dpad_y = joy_msg.axes[7]       # Down (-1), Up (+1)
         l_bumper = joy_msg.buttons[4]
 
-       
+        # Buttons
+        torque_reset = joy_msg.buttons[1]
+
+        wrist1_pos, dxl_comm_result, dxl_error = self.packet_handler.read4ByteTxRx(
+            self.port_handler, WRIST1_ID, ADDR_PRESENT_POSITION
+        )
+        wrist2_pos, dxl_comm_result, dxl_error = self.packet_handler.read4ByteTxRx(
+            self.port_handler, WRIST2_ID, ADDR_PRESENT_POSITION
+        )
+
+        # Reset torque
+        if torque_reset:
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, WRIST1_ID, ADDR_TORQUE_ENABLE, not TORQUE_ENABLE
+            )
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, WRIST2_ID, ADDR_TORQUE_ENABLE, not TORQUE_ENABLE
+            )
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, ELBOW_ID, ADDR_TORQUE_ENABLE, not TORQUE_ENABLE
+            )
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, SHOULDER1_ID, ADDR_TORQUE_ENABLE, not TORQUE_ENABLE
+            )
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, SHOULDER2_ID, ADDR_TORQUE_ENABLE, not TORQUE_ENABLE
+            )
+
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, WRIST1_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE
+            )
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, WRIST2_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE
+            )
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, ELBOW_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE
+            )
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, SHOULDER1_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE
+            )
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(
+                self.port_handler, SHOULDER2_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE
+            )
+
 
         # TODO: Gotta do math stuff so wrist motors move properly
         # issue of creating individual dynamixel movement, rather than one input moving both motors at the same time,
         # which does not allow rotation, only up and down movement 
         velocity_elbow = int(interp(dpad_x, [-1, 1], [MIN_VELOCITY, MAX_VELOCITY])) # this works as there is only one motor on this particular joint
         if bool(l_bumper):
-            position_wrist1 = int(interp(y_axis, [-1, 1], [MIN_POSITION, MAX_POSITION]))
+            velocity_wrist1 = int(interp(y_axis, [-1, 1], [MIN_VELOCITY, MAX_VELOCITY]))
+            velocity_wrist2 = int(interp(-y_axis, [-1, 1], [MIN_VELOCITY, MAX_VELOCITY]))
             velocity_shoulder1 = int(interp(right_y, [-1, 1], [MIN_VELOCITY, MAX_VELOCITY]))
             velocity_shoulder2 = 0
         else:
-            # if y_axis != 0:
-            #     position_wrist1 = int(interp(y_axis, [-1, 1], [MIN_POSITION, MAX_POSITION]))
-            #     position_wrist2 = int(interp(y_axis, [-1, 1], [MIN_POSITION, MAX_POSITION]))
             velocity_wrist1 = int(interp(y_axis, [-1, 1], [MIN_VELOCITY, MAX_VELOCITY]))
             velocity_wrist2 = int(interp(y_axis, [-1, 1], [MIN_VELOCITY, MAX_VELOCITY]))
             velocity_shoulder1 = int(interp(right_y, [-1, 1], [MIN_VELOCITY, MAX_VELOCITY]))
             velocity_shoulder2 = int(interp(right_y, [-1, 1], [MIN_VELOCITY, MAX_VELOCITY]))
 
         # Write velocity to motor
-        # dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-        # 	self.port_handler, WRIST1_ID, ADDR_GOAL_POSITION, self._to_twos_complement(position_wrist1, 4)
-        # )
-        # dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-        # 	self.port_handler, WRIST2_ID, ADDR_GOAL_POSITION, self._to_twos_complement(position_wrist2, 4)
-        # )
         dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-            self.port_handler, WRIST1_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_wrist1, 4)
+        	self.port_handler, WRIST1_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_wrist1, 4)
         )
         dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-            self.port_handler, WRIST2_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_wrist2, 4)
+        	self.port_handler, WRIST2_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_wrist2, 4)
         )
         dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
             self.port_handler, ELBOW_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_elbow, 4)
         )
-        dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-            self.port_handler, SHOULDER1_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_shoulder1, 4)
-        )
-        dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
-            self.port_handler, SHOULDER2_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_shoulder2, 4)
-        )
+        # dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
+        #     self.port_handler, SHOULDER1_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_shoulder1, 4)
+        # )
+        # dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(
+        #     self.port_handler, SHOULDER2_ID, ADDR_GOAL_VELOCITY, self._to_twos_complement(velocity_shoulder2, 4)
+        # )
 
 
         if dxl_comm_result != COMM_SUCCESS:
@@ -162,8 +199,8 @@ class DynamixelTeleop(Node):
         elif dxl_error != 0:
             self.get_logger().error(f"Dynamixel error: {self.packet_handler.getRxPacketError(dxl_error)}")
         else:
-            self.get_logger().info(f"Position Command Sent for Wrist 1: {position_wrist1}")
-            self.get_logger().info(f"Position Command Sent for Wrist 2: {position_wrist2}")
+            self.get_logger().info(f"Position Command Sent for Wrist 1: {velocity_wrist1}")
+            self.get_logger().info(f"Position Command Sent for Wrist 2: {velocity_wrist2}")
             self.get_logger().info(f"Velocity Command Sent for Elbow : {velocity_elbow}")
             self.get_logger().info(f"Velocity Command Sent for Shoulder 1 : {velocity_shoulder1}")
             self.get_logger().info(f"Velocity Command Sent for Shoulder 2 : {velocity_shoulder2}")
